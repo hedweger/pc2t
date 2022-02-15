a253f98fe3f3a1c6bac5655b4c8ec5d6afceb887
hedweger <73221181+hedweger@users.noreply.github.com>
2022-02-15 23:01:01 +0100

    final commit

--- cv-02/Database.java
+++ cv-02/Database.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Database {
+
+
+public class Database {
+    private final String Name;
+    private final int Birth;
+    private float Pay;
+    static float MaximumPay = 1;
+
+    public Database(String Name, int Birth)
+    {
+        this.Name = Name;
+        this.Birth = Birth;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public int getBirth()
+    {
+        return Birth;
+    }
+
+    public float getPay()
+    {
+        return Pay;
+    }
+
+    public float getMaximumPay() { return MaximumPay; }
+
+    public static void SetMaxPay(float MaxPay) { MaximumPay = MaxPay;}
+
+    public boolean PayAdd(float Pay)
+    {
+        if (this.Pay + Pay < MaximumPay)
+        {
+            this.Pay += Pay;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }

d24cd42971310d6336d80010ce95792b57350c90
hedweger <73221181+hedweger@users.noreply.github.com>
2022-03-09 09:37:26 +0100

    Delete cv-04-2/src directory

--- cv-04-2/src/NoMeanException.java
+++
@@ -1,8 +0,0 @@
-public class NoMeanException extends java.lang.Exception {
-    public NoMeanException() {
-        super("Nebyl zadán žádný průměr");
-    }
-    public NoMeanException(String a) {
-        super(a);
-    }
-}
